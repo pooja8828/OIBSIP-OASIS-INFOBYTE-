5141d8a7f06ed17a7cbb5fe0e4ae79838dba7965
pooja8828 <pm8828339585@gmail.com>
2025-12-09 19:28:33 +0530

    Landing-Page
    # 🌐 Landing Page – OIBSIP (Oasis Infobyte) Internship  
    This is a fully responsive and clean **Landing Page** created as a part of the **Oasis Infobyte Web Development Internship – Level 1, Task 1**.
    
    ---
    
    ## 📌 Project Overview
    This landing page is built using **HTML, CSS, and JavaScript**.  
    It is designed to demonstrate:
    
    - Basic webpage layout  
    - Navigation bar  
    - Hero section  
    - Buttons & hover effects  
    - Responsive design  
    - Clean and modern UI  
    
    No frameworks were used — the project is made **purely with HTML, CSS, and JS.**
    
    ---
    
    ## 🛠️ Technologies Used
    - **HTML5**
    - **CSS3**
    - **JavaScript (ES6)**
    
    ---
    
    ## 📁 File Structure

---
+++ lp.js
@@ -0,0 +1,8 @@
+
+/* ===================== script.js ===================== */
+
+// Simple button click effect
+const ctaBtn = document.querySelector(".cta-btn");
+ctaBtn.addEventListener("click", () => {
+    alert("Welcome to Oasis Infobyte Landing Page!");
+});
